01afd35b27c3269d9994c7c4b129be8ccfbfac3a
Vladislav <vladdislav.georgiev@gmail.com>
2018-07-17 21:38:43 +0300

    fixing merge bugs

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 from flask import Flask, flash, g, redirect, render_template, request, session, url_for, send_from_directory
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import json
+
 app = Flask(__name__, static_url_path='')
+
+app.secret_key = "super secret"
+
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
+
+cred = credentials.Certificate("./config/secrets/serviceAccountKey.json")
+firebase_admin.initialize_app(cred, options={"databaseURL":"https://tetrix-1d1fc.firebaseio.com/"})
+
+users = db.reference('users')
+
+@socketio.on('keypress')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
 
 @app.route('/lib/<path:path>')
 def send_js(path):
@@ -9,14 +32,7 @@ def send_js(path):
 @app.route('/data/<path:path>')
 def send_data(path):
     return send_from_directory('static/data', path)
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-import json
-app.secret_key = "super secret"
-cred = credentials.Certificate("./config/secrets/serviceAccountKey.json")
-firebase_admin.initialize_app(cred, options={"databaseURL":"https://tetrix-1d1fc.firebaseio.com/"})
-users = db.reference('users')
+
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -36,6 +52,7 @@ def register():
         flash(error)
         
     return render_template('register.html')
+    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -43,17 +60,12 @@ def login():
         password = request.form['password']
         error = None
         logging_user = list(users.order_by_child('username').equal_to(username).get().items())
-        session.clear()
-        session['user_id'] = logging_user[0][0]
         if error == None and check_password_hash(logging_user[0][1]['password'], password):
-            return "Logged!"
+            session.clear()
+            session['user_id'] = logging_user['id']
+            return redirect(url_for('index'))
         flash(error)
     return render_template('login.html')
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
 
         #flash(error)
 
