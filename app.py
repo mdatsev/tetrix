f476cc6fedc8f90ed8a0e7d0f6ea5a7dfea3adcd
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-07-17 16:03:52 +0300

    Yeet

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, flash, g, redirect, render_template, request, session, url_for
 app = Flask(__name__)
 
 @app.route('/')
