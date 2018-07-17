b45631ec02ba581ef348fe0c000b1a6d79c26360
VGeorgiev1 <vladdislav.georgiev@gmail.com>
2018-07-17 16:57:13 +0300

    fixing flash

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from flask import Flask, flash, g, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
-app = Flask(__name__)
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+app = Flask(__name__)
+app.secret_key = "super secret"
 cred = credentials.Certificate("./config/secrets/serviceAccountKey.json")
 firebase_admin.initialize_app(cred, options={"databaseURL":"https://tetrix-1d1fc.firebaseio.com/"})
 users = db.reference('users')
-@app.route('/')
-def hello():
-    return 'Hello, World'
-
-@app.route('/hello',  methods=['GET'])
-def index():
-    return 'Index Page'
-
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -26,11 +19,13 @@ def register():
         elif not password:
             error = 'Password is required'
         users.push({
-            "username":username,
-            "password":generate_password_hash(password)
+            "username": username,
+            "password": generate_password_hash(password)
         })
         if error is None:
             return redirect(url_for('.login'))
+        flash(error)
+        
     return render_template('register.html')
 @app.route('/login', methods=('GET', 'POST'))
 def login():
@@ -40,5 +35,5 @@ def login():
         error = None
         if error == None and check_password_hash(list(users.order_by_child('username').equal_to(username).get().items())[0][1]['password'], password):
             return "Logged!"
-        #flash(error)
+        flash(error)
     return render_template('login.html')
