919216dbf2a772268524b16ff4bc5144f011b731
NotGayBut5CentsAre5Cents <telerik007@gmail.com>
2018-07-17 16:03:37 +0300

    testing login and register

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +8,36 @@ def hello():
 @app.route('/hello',  methods=['GET'])
 def index():
     return 'Index Page'
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required'
+        flash(error)
+        if error is None:
+            return redirect(url_for('.login', username=username, password=generate_password_hash(password)))
+    return render_template('register.html')
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    usernameRedir = request.args['username']
+    passwordRedir = request.args['password']
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('login.html', username=usernameRedir, password= passwordRedir)
+app.run()
