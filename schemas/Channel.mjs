a8af4d75531e4167319cf20e2a71a75db297ca56
pepy3 <pepy_3@abv.bg>
2018-07-24 16:43:27 +0300

    add chat

---
+++ schemas/Channel.mjs
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const ObjectId= Schema.Types.ObjectId
+const channelSchema = new Schema({
+  name:String,
+  max_players:Number,
+  players:[{type:ObjectId, ref:"User"}],
+  messages:[{author:String, message:String}],
+});
+
+channelSchema.method('test', () => {
+
+})
+
+const Channel =  mongoose.model("Channel", channelSchema)
+
+export default Channel;
